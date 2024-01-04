b417912630ac5fc6f002c3a91450fa8e075f8ba4
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2024-01-04 22:22:23 +0700

    Connect listener and broadcaster thread

--- app/server.c
+++ app/server.c
@@ -16,6 +16,7 @@ typedef struct Connection{
 int channel[2];
 #define READ_END 0
 #define WRITE_END 1
+#define BUFFER_SIZE 128
 
 typedef struct ThreadParam{
     int file_descriptor;
@@ -25,11 +26,33 @@ typedef struct PipeProduct{
 
 } PipeProduct;
 
-void* runner(void* rawParam){
+void* listener(void* rawParam){
     ThreadParam* param = rawParam;
     int connection = param->file_descriptor;
 
-    write(connection, "Hello, world", 1);
+    printf("Connected\n");
+    char buffer[BUFFER_SIZE];
+    while(1){
+        int count = read(connection, buffer, BUFFER_SIZE - 2);
+        if(count == 0) break;
+
+        write(channel[WRITE_END], buffer, count);
+        fflush(fdopen(channel[WRITE_END], "w"));
+    }
+    printf("Disconnected\n");
+
+    return 0;
+}
+
+void* broadcaster(void* _){
+    while(1){
+        char buffer[BUFFER_SIZE];
+        int count = read(channel[READ_END], buffer, BUFFER_SIZE - 2);
+        buffer[count] = '\0';
+        printf("Incoming (%d): %s", count, buffer);
+        fflush(stdout);
+        fflush(stdout);
+    }
     return 0;
 }
 
@@ -43,6 +66,7 @@ int main(int argc, char** argv){
     pipe(channel);
 
     // Setting up for TCP connection
+    int err;
     int port = atoi(argv[1]);
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_address = {
@@ -50,9 +74,26 @@ int main(int argc, char** argv){
         .sin_port = htons(port),
         .sin_addr.s_addr = INADDR_ANY
     };
-    (void)bind(server_socket, (struct sockaddr*) (&server_address), sizeof(server_address));
-    listen(server_socket, 16);
 
+    err = bind(server_socket, (struct sockaddr*) (&server_address), sizeof(server_address));
+    if(err == -1){
+        printf("Can't bind server\n");
+        return 0;
+    }
+
+    err = listen(server_socket, 16);
+    if(err == -1){
+        printf("Can't listen to port\n");
+        return 0;
+    }
+
+    // One thread for broadcasting all data
+    pthread_t broadcastThread;
+	pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_create(&broadcastThread, &attr, broadcaster, NULL);
+
+    // Main thread manage initial connection 
     while(1){
         int connection = accept(server_socket, NULL, NULL);
         ThreadParam param = {
@@ -62,7 +103,7 @@ int main(int argc, char** argv){
 		pthread_t tid;
 		pthread_attr_t attr;
 		pthread_attr_init(&attr);
-		pthread_create(&tid, &attr, runner, &param);
+		pthread_create(&tid, &attr, listener, &param);
 	}
 
     return 0;
